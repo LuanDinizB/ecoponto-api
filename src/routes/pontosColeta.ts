cdc12e51c8d7133ec21b72ef0318f78baaa66a68
Luan Diniz Barreiros <luanminasgerais@hotmail.com>
2026-04-07 15:56:21 -0300

    refactor: adiciona middleware de upload nas rotas de pontos de coleta

--- src/routes/pontosColeta.ts
+++ src/routes/pontosColeta.ts
@@ -8,16 +8,19 @@ import {
   deletarPonto,
 } from '../controllers/pontoColetaController';
 import { autenticar, apenasCooperativa } from '../middlewares/auth';
+import { upload } from '../middlewares/upload';
 
 const router = Router();
 
 
 router.get('/', listarPontos);
 router.get('/:id', getPonto);
+
 router.use(autenticar, apenasCooperativa);
+
 router.get('/meus/lista', getMeusPontos);
-router.post('/', criarPonto);
-router.put('/:id', atualizarPonto);
+router.post('/', upload.single('imagem'), criarPonto);
+router.put('/:id', upload.single('imagem'), atualizarPonto);
 router.delete('/:id', deletarPonto);
 
 export default router;
